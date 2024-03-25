c80e810cb5e0c341923ee11a91fba097b6cef136
Ankur Prajapati <91510065+Ankur77720@users.noreply.github.com>
2024-03-25 17:52:31 +0530

    file code added

--- src/app/home/library/page.tsx
+++ src/app/home/library/page.tsx
@@ -1,9 +1,65 @@
-import React from 'react'
+"use client"
+
+import React, { useRef, useState } from 'react';
+import axios from '../../../../extends/axios';
+import { useRouter } from 'next/navigation';
 
 const Library = () => {
-    return (
-        <div>Library</div>
-    )
-}
+    const fileInputRef = useRef<HTMLInputElement>(null);
+    const router = useRouter();
 
-export default Library
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        if (fileInputRef.current?.files) {
+            const audioFiles = Array.from(fileInputRef.current.files).filter(file => file.type.startsWith('audio/'));
+            if (audioFiles.length > 0) {
+                const formData = new FormData();
+                audioFiles.forEach(file => formData.append('audioFiles', file));
+                axios.post('/upload', formData)
+                    .then(response => {
+                        console.log(response.data)
+                        console.log("Song uploaded successfully!");
+                        router.push('/home');
+                    })
+                    .catch(error => {
+                        // Handle error
+                        console.error(error);
+                    });
+
+            }
+        }
+    };
+
+    const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files) {
+            const files = Array.from(event.target.files);
+            setSelectedFiles(files);
+        }
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="file" accept="audio/*" ref={fileInputRef} multiple onChange={handleFileChange} />
+                <button type="submit">Submit</button>
+            </form>
+            {selectedFiles.length > 0 && (
+                <div>
+                    <h3>Selected Files:</h3>
+                    <ul>
+                        {selectedFiles.map((file, index) => (
+                            <li key={index}>{file.name}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
+
+
+};
+
+
+export default Library;
